db1551cc3ce6c0782b90ba4b9957bd18aa6fdeff
alona77 <alonacojocarescu@gmail.com>
2021-10-20 11:34:29 +0300

    rezv pana la a treia pb

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#from os import read
 def show_menu():
     print("1. Citire lista")
     print("2. Afisarea partii intregi a fiecarui numar din lista")
@@ -6,17 +7,63 @@ def show_menu():
     print("5. Inlocuire nr cu un sir de string-uri care le descriu")
     print("6. Iesire meniu")
 
+
+def read_list():
+    floats = []
+    float_as_str = input ("Introduceti numerele prin spatiu:")
+    float_as_str_split = float_as_str.split(' ')
+    for num_str in float_as_str_split:
+        floats.append(float(num_str))
+    return floats
+
+def get_integers(lst):
+    """
+    Afiseaza partea intreaga a nr din lista
+    param intrare: lista de float-uri
+    return: lista de intregi
+    """
+    result=[]
+    for elem in lst:
+        result.append(int(elem))
+    return result
+
+def test_get_integers():
+    assert get_integers([1.2, 3.89, 1.0, 7.7]) == [1, 3, 1, 7]
+    assert get_integers([]) == []
+    assert get_integers([15.9, 28.7, 13.2]) == [15, 28, 13]
+
+def get_elem_interval(lst, first , second):
+    """
+    Determina nr care apartin intervalului deschis (first, second)
+    param intrare: lista floatu-uri, first capatul inferior al intervalului, iar second capatul superior
+    return: lista elem care respecta conditia
+    """
+    result=[]
+    for i in lst:
+        if i > first and i < second:
+            result.append(i)
+    return result
+
+def test_get_elem_interval():
+    assert get_elem_interval([1.3, 2.7, 28], 1, 10) == [1.3, 2.7]
+    assert get_elem_interval([5.3, 7.9, 11.6], 4, 6) == [5.3]
+    assert get_elem_interval([2, 6.90, 10], 3, 4) == []
+
 def main():
     lst=[]
     while True:
         show_menu()
         opt=int(input("Introductei optiunea: "))
         if opt == 1:
-            pass
+            lst=read_list()
         elif opt == 2:
-            pass
+            result=get_integers(lst)
+            print(f'Lista de intregi obtinuta din lista initiala este {result}')
         elif opt == 3:
-            pass
+            first=int(input("Introduceti marginea inferioara a intervalului: "))
+            second=int(input("Introduceti marginea superioara a intervalului: "))
+            result=get_elem_interval(lst, first, second)
+            print(f'Numerele incluse in intervalul cerut sunt {result}')
         elif opt == 4:
             pass
         elif opt == 5:
@@ -27,4 +74,6 @@ def main():
             print("Optiune invalida")
     
 if __name__ == '__main__':
+    test_get_integers()
+    test_get_elem_interval()
     main()
